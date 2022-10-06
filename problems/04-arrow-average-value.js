40d20b0445db2e184cb85855612d1339c28b3c05
Rachel <rachel@pop-os.localdomain>
2022-10-06 16:56:02 -0400

    04 arrow average

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -10,7 +10,24 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// Your code here
+
+
+const arrowAvgValue = (array) => {
+  let total = 0;
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    let ele = array[i];
+    sum += ele }
+    console.log("sum:", sum)
+    let avg = sum / array.length
+      console.log("avg:", avg)
+      console.log("array.l:", array.length)
+    total += avg
+
+  return total
+}
+arrowAvgValue([10, 20]); // => 15
+arrowAvgValue([2, 3, 7]); // => 4
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
